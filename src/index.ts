84a3d3c8646316d63fea9e1d75b9309a6afcf57b
RangHo Lee <hello@rangho.me>
2025-11-12 16:58:26 +0900

    feat: implement reverse proxy

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,37 @@
+const baseUrl = "by.rangho.dev";
+
 const routes = [
   {
-    src: "jumpstart",
-    dst: "https://rangho-jumpstart.vercel.app/api/",
+    src: "/jumpstart",
+    dst: "https://rangho-jumpstart.vercel.app/api",
+  },
+  {
+    src: "/repository",
+    dst: "https://rangho-repository.netlify.app",
   },
 ];
 
+async function work(request: Request, _env: Env, _ctx: ExecutionContext): Promise<Response> {
+  const requestUrl = new URL(request.url);
+  if (requestUrl.host !== baseUrl) {
+    return Response.redirect(requestUrl.href, 302);
+  }
+  
+  const requestSrc = requestUrl.pathname;
+
+  const target = routes.find(route => requestSrc.startsWith(route.src));
+  if (!target) {
+    return new Response(`Unknown route: ${requestSrc}`, {
+      status: 404,
+      statusText: "Not Found",
+    });
+  }
+
+  const subdirectory = requestSrc.replace(`${target.src}`, "");
+  const responseUrl = new URL(target.dst + subdirectory + requestUrl.search);
+    return await fetch(responseUrl);
+}
+
 export default {
-  async fetch(request, env, ctx): Promise<Response> {
-    return new Response("Hello World!");
-  },
+  fetch: work
 } satisfies ExportedHandler<Env>;
